f0684fc1100b8a5982c4a6c8215d458b037f0bc1
Clebson Torres <104043419+Clebson-Torres@users.noreply.github.com>
2024-07-27 20:00:04 -0300

    Update home.py
    solve error !Errno 32] Broken pipe"

--- home.py
+++ home.py
@@ -3,6 +3,7 @@ import yt_dlp
 import os
 import tempfile
 import shutil
+import io
 
 def download_youtube_video(url, max_height=1080):
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -16,14 +17,14 @@ def download_youtube_video(url, max_height=1080):
                 info = ydl.extract_info(url, download=True)
                 filename = ydl.prepare_filename(info)
                 if os.path.exists(filename):
-                    # Copia o arquivo para um local temporário que o Streamlit pode acessar
-                    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
-                    shutil.copy2(filename, temp_file.name)
-                    return True, temp_file.name, ""
+                    # Lê o arquivo em memória
+                    with open(filename, "rb") as file:
+                        file_data = file.read()
+                    return True, file_data, os.path.basename(filename), ""
                 else:
-                    return False, "", f"Arquivo não encontrado: {filename}"
+                    return False, None, "", f"Arquivo não encontrado: {filename}"
         except Exception as e:
-            return False, "", str(e)
+            return False, None, "", str(e)
 
 st.title('YouTube Video Downloader')
 
@@ -33,24 +34,19 @@ max_resolution = st.selectbox('Selecione a resolução máxima:', [1080, 720, 48
 if st.button('Preparar Download'):
     if url:
         with st.spinner('Preparando o vídeo para download...'):
-            success, file_path, error_message = download_youtube_video(url, max_resolution)
+            success, file_data, file_name, error_message = download_youtube_video(url, max_resolution)
             
             if success:
                 st.success('Vídeo pronto para download!')
                 try:
-                    with open(file_path, "rb") as file:
-                        st.download_button(
-                            label="Baixar Vídeo",
-                            data=file,
-                            file_name=os.path.basename(file_path),
-                            mime="video/mp4"
-                        )
+                    st.download_button(
+                        label="Baixar Vídeo",
+                        data=io.BytesIO(file_data),
+                        file_name=file_name,
+                        mime="video/mp4"
+                    )
                 except Exception as e:
                     st.error(f"Erro ao preparar o download: {str(e)}")
-                finally:
-                    # Limpa o arquivo temporário
-                    if os.path.exists(file_path):
-                        os.unlink(file_path)
             else:
                 st.error(f'Erro ao preparar o vídeo: {error_message}')
     else:
